8342e48b93f547802306e1f078f70353debefede
vijay.k.arora <vijayarora325@gmail.com>
2024-04-18 18:45:36 +0200

    added kafka embedded tests

--- src/test/java/at/rewe/assignment/infrastructure/KafkaEmbeddedTest.java
+++ src/test/java/at/rewe/assignment/infrastructure/KafkaEmbeddedTest.java
@@ -1,11 +1,11 @@
 package at.rewe.assignment.infrastructure;
 
 import at.rewe.assignment.entity.Email;
+import at.rewe.assignment.infrastructure.kafka.email.producer.EmailProducer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.messaging.handler.annotation.Header;
@@ -18,22 +18,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class KafkaEmbeddedTest {
 
     @Autowired
-    private KafkaTemplate<String, Email> kafkaTemplate;
+    private EmailProducer emailProducer;
 
     @Test
     public void testSendMessage() {
         Email email = new Email();
         email.setSender("user1234@gmail.com");
-        kafkaTemplate.send("emails", "gmail.com", email);
+        emailProducer.sendMessage(email);
 
         email.setSender("user1234@outlook.com");
-        kafkaTemplate.send("emails", "outlook.com", email);
+        emailProducer.sendMessage(email);
 
         email.setSender("user1234@hotmail.com");
-        kafkaTemplate.send("emails", "hotmail.com", email);
+        emailProducer.sendMessage(email);
     }
 
-    @KafkaListener(topics = "topicName", groupId = "testGroup")
+    @KafkaListener(topics = "topicName", groupId = "email-consumer-group")
     public void listen(@Payload Email email, @Header(KafkaHeaders.RECEIVED_PARTITION) int partition) {
         assertEquals("user1234@"+ (partition == 0 ? "gmail.com" : partition == 1 ? "outlook.com" : partition == 2 ? "hotmail.com" : null), email.getSender());
     }
